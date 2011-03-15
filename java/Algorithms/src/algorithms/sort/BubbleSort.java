c68d20d2ad82cb7da2feba32684084cb50eed98a
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-03-15 12:16:10 +0000

    add test and bubble sort

--- java/Algorithms/src/algorithms/sort/BubbleSort.java
+++ java/Algorithms/src/algorithms/sort/BubbleSort.java
@@ -1,10 +1,22 @@
 package algorithms.sort;
 
 /**
- *
+ * 
  * @author leeing
  * @date Mar 15, 2011
  */
 public class BubbleSort {
+
+    public static void bubbleStor(Comparable a[]){
+        for(int i = 0;i<a.length;i++){
+            for(int j = 0;j<a.length-i-1;j++){
+                if(a[j].compareTo(a[j+1])>0){
+                    Comparable temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
 
 }
