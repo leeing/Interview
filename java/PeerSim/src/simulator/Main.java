0114a3e6ac1a2c05d8d30fb829db76c8addfa2c3
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-03-15 11:27:57 +0000

    PeerSim Simulator 1.0.5

---
+++ java/PeerSim/src/simulator/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simulator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author leeing
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<Node> nodes = new ArrayList<Node>();
+        for(int i = 0;i<1000;i++){
+            nodes.add(new Node(i));
+        }
+
+        for(int i = 0;i<1000;i++){
+            
+        }
+
+
+    }
+
+}
