a313a5bbfbcf09faba468ef178a674445be14929
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-03-15 03:26:25 +0000

    add other sorts.

---
+++ java/Algorithms/src/algorithms/sort/MergeSort.java
@@ -0,0 +1,10 @@
+package algorithms.sort;
+
+/**
+ *
+ * @author leeing
+ * @date Mar 15, 2011
+ */
+public class MergeSort {
+
+}
--- java/Algorithms/src/algorithms/sort/ShellSort.java
+++ java/Algorithms/src/algorithms/sort/ShellSort.java
@@ -14,8 +14,18 @@ public class ShellSort {
         ArrayUtil.print(array);
     }
 
-    public static void shellSort(Comparable[]a){
-        
+    public static void shellSort(Comparable[] a) {
+        for (int gap = a.length / 2; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < a.length; i++) {
+                Comparable key = a[i];
+                int j = i;
+                // 注意这里是用 key 和 a[j-gap] 进行比较
+                for (; j >= gap && key.compareTo(a[j - gap]) < 0; j = j - gap) {
+                    a[j] = a[j - gap];
+                }
+                a[j] = key;
+            }
+        }
     }
 
 }
