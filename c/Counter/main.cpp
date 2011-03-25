7593da8b0f4eea11af72027bcc05dcf3dd5f9d2d
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-03-25 02:52:06 +0000

    sum counters.

---
+++ c/Counter/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: leeing
+ *
+ * Created on March 24, 2011, 2:56 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+int counter(int num,int max);
+using namespace std;
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int num;
+    cout<<"input a num:"<<endl;
+    cin>>num;
+    int result = counter(num,num);
+    cout<<"The result is "<<result<<endl;
+    return EXIT_SUCCESS;
+}
+
+int counter(int num,int max){
+    if(max == 1){
+        return 1;
+    }
+    if(max>num){
+        return counter(num,num);
+    }
+
+    if(num == max){
+        return counter(num,max-1) + 1;
+    }else{
+        return counter(num,max-1)+counter(num-max,max);
+    }
+
+}
