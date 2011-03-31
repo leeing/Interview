ad7724a2b60ab5c533997d9f976fd21c4f291cd3
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-03-31 02:08:52 +0000

    atexit( ) function execute after the main function.

---
+++ c/ExecAfterMain/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: leeing
+ *
+ * Created on March 31, 2011, 10:02 AM
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+int atexit(void (*function)(void));
+void fn1(void);
+
+int value = 10;
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    atexit(fn1);
+    int value = 100;
+    printf("::value is %d\n",::value);
+    printf("local value is %d\n",value);
+    printf("This is executed first.\n");
+    return 0;
+}
+
+void fn1(){
+    printf("next.\n");
+} 
---
+++ c/ExecAfterMain/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=execaftermain
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/execaftermain
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=execaftermain.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/execaftermain.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=execaftermain
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/execaftermain
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=execaftermain.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/execaftermain.tar
