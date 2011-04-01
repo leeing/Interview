f2dac90c367b822fea4fc767184d54bf80eefb29
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-01 02:42:39 +0000

    hadoop max temp.

---
+++ java/MaxTemp/src/org/leeing/hadoop/maxtemp/MaxTemperature.java
@@ -0,0 +1,36 @@
+package org.leeing.hadoop.maxtemp;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ *
+ * @author leeing
+ */
+public class MaxTemperature {
+    public static void main(String[] args) throws Exception{
+
+    String from ="hdfs://localhost:54310/user/hadoop/input.txt";
+    String to =  "hdfs://localhost:54310/user/hadoop/output";
+
+    Job job = new Job();
+    job.setJarByClass(MaxTemperature.class);
+
+    FileInputFormat.addInputPath(job, new Path(from));
+    FileOutputFormat.setOutputPath(job, new Path(to));
+
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setReducerClass(MaxTemperatureReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+
+}
---
+++ java/MaxTemp/src/org/leeing/hadoop/maxtemp/MaxTemperatureReducer.java
@@ -0,0 +1,29 @@
+/*
+ * MaxTemperatureReducer.java
+ *
+ * Created on Apr 13, 2010, 5:04:50 PM
+ */
+package org.leeing.hadoop.maxtemp;
+
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+/**
+ *
+ * @author leeing
+ */
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
+    }
+}
