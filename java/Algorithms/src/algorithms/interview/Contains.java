da152b5776ce7fd1fd8188f7d723dbf182d0da67
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-02 14:02:17 +0000

    add wordcount2

---
+++ java/Algorithms/src/algorithms/interview/Contains.java
@@ -0,0 +1,17 @@
+package algorithms.interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @date Apr 2, 2011
+ * @author leeing
+ */
+public class Contains {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        int t1 = 10;
+        list.add(10);
+        System.out.println(list.contains(t1));
+    }
+}
