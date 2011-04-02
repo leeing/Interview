95b2f292db13b6ca16194a462f5947381a515a0d
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-02 14:05:27 +0000

    list.remove

--- java/Algorithms/src/algorithms/interview/Contains.java
+++ java/Algorithms/src/algorithms/interview/Contains.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * question : list.remove(Object) and list.remove(int)
  * @date Apr 2, 2011
  * @author leeing
  */
 public class Contains {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<Integer>();
-        int t1 = 10;
-        list.add(10);
-        System.out.println(list.contains(t1));
+        for(int i = 0;i<=10;i++){
+            list.add(i);
+        }
+//        list.remove(10);
+        Integer i = list.remove(10);
+        
+        System.out.println(list.contains(10));
     }
 }
