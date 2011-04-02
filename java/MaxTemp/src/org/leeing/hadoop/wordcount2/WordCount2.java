bb8f356a65278603b7880bf3b332a459171bc8fe
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-02 14:02:31 +0000

    add wordcount2

--- java/MaxTemp/src/org/leeing/hadoop/wordcount2/WordCount2.java
+++ java/MaxTemp/src/org/leeing/hadoop/wordcount2/WordCount2.java
@@ -1,6 +1,14 @@
 package org.leeing.hadoop.wordcount2;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -10,7 +18,26 @@ import org.apache.hadoop.util.ToolRunner;
  */
 public class WordCount2 extends Configured implements Tool{
 
-    
+    public static class Map extends Mapper<LongWritable, Text, Text, Object>{
+        static enum Counter {INPUT_WORDS}
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        private boolean caseSensitive = true;
+        private Set<String> patternsToSkip = new HashSet<String>();
+        private long numRecords = 0;
+        private String inputFile;
+
+        @Override
+        public void setup(Context context){
+            Configuration conf = context.getConfiguration();
+            caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+            inputFile = conf.get("mapreduce.map.input.file");
+            if(conf.getBoolean("wordcount.skip.patterns", false)){
+                Path[] patternFiles = new Path[0];
+            }
+        }
+
+    }
     
     public int run(String[] strings) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
