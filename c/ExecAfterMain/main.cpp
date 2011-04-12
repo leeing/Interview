9b3066290fddc8a5bc537bb2673907a465a87922
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-12 09:34:35 +0000

    add empty struct and class

--- c/ExecAfterMain/main.cpp
+++ c/ExecAfterMain/main.cpp
@@ -13,18 +13,26 @@ void fn1(void);
 
 int value = 10;
 using namespace std;
+struct student{
 
+}stu;
+
+class A{
+};
 /*
  * 
  */
 int main(int argc, char** argv) {
-    atexit(fn1);
-    int value = 100;
-    printf("::value is %d\n",::value);
-    printf("local value is %d\n",value);
-    printf("This is executed first.\n");
+//    atexit(fn1);
+//    int value = 100;
+//    printf("::value is %d\n",::value);
+//    printf("local value is %d\n",value);
+//    printf("This is executed first.\n");
+//
+//    printf("this is v~\v");
 
-    printf("this is v~\v");
+    printf("the size of empty struct is: %d\n",sizeof(stu));
+    printf("the size of empty class is :%d\n",sizeof(A));
     return 0;
 }
 
