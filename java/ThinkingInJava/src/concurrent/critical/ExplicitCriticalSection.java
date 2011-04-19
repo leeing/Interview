7b8091a215e086e194fd3e12067559fd4f63210b
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-19 11:12:53 +0000

    something wrong with these codes.

---
+++ java/ThinkingInJava/src/concurrent/critical/ExplicitCriticalSection.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurrent.critical;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ExplicitPairManager1 extends PairManager {
+
+    private Lock lock = new ReentrantLock();
+
+    public synchronized void increment() {
+        lock.lock();
+        try {
+            p.incrementX();
+            p.incrementY();
+            store(getPair());
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+class ExplicitPairManager2 extends PairManager {
+
+    private final Lock lock = new ReentrantLock();
+
+    @Override
+    public void increment() {
+        Pair temp;
+        lock.lock();
+        try {
+            p.incrementX();
+            p.incrementY();
+            temp = getPair();
+        } finally {
+            lock.unlock();
+        }
+        store(temp);
+    }
+}
+
+public class ExplicitCriticalSection {
+
+    public static void main(String[] args) throws Exception {
+        PairManager pman1 = new ExplicitPairManager1(),
+                pman2 = new ExplicitPairManager2();
+        CriticalSection.testApproaches(pman1, pman2);
+    }
+}
