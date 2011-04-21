05d5fe9ef06674efa14f63ae0b6dbaf06493b6de
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-21 08:32:34 +0000

    C/C++ interview questions

---
+++ c/Interview/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: leeing
+ *
+ * Created on April 21, 2011, 4:24 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ c/Interview/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW_TDM-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_TDM-Windows
+CND_ARTIFACT_NAME_Debug=interview
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_TDM-Windows/interview
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_TDM-Windows/package
+CND_PACKAGE_NAME_Debug=interview.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_TDM-Windows/package/interview.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW_TDM-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW_TDM-Windows
+CND_ARTIFACT_NAME_Release=interview
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW_TDM-Windows/interview
+CND_PACKAGE_DIR_Release=dist/Release/MinGW_TDM-Windows/package
+CND_PACKAGE_NAME_Release=interview.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW_TDM-Windows/package/interview.tar
