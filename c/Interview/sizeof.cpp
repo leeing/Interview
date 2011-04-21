d5197e263ab9d3f7efa7f95e457173f451b6c008
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-04-21 10:30:16 +0000

    note that sizeof(long) is 4.

--- c/Interview/sizeof.cpp
+++ c/Interview/sizeof.cpp
@@ -14,14 +14,14 @@ struct A {
     char t : 4; // 注意冒号起的作用
     char k : 4;
     unsigned short i : 8;
-    unsigned long m;
+    unsigned long m; // long 占据 4 个字节 
 } a;
 
 struct B {
     char t ;
     char k ;
     short i ;
-    long m;
+//    int m;
 } b;
 
 /*
@@ -31,6 +31,12 @@ int main(int argc, char** argv) {
 
     cout<<"size of struct A is:"<<sizeof(a)<<endl;
     cout<<"size of struct B is:"<<sizeof(B)<<endl;
+    cout<<"size of char is :"<<sizeof(char)<<endl;
+    cout<<"size of short is :"<<sizeof(short)<<endl;
+
+    int size = sizeof(unsigned long);
+    cout<<"size of long is："<<size<<endl;
+    cout<<endl;
     return 0;
 }
 
