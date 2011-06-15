a0bad8f33026f408b0f20a8631d0fd46e3bd93c4
leeingbupt <leeingbupt@7b4cf52a-4541-bc81-1ac6-210699af4094>
2011-06-15 13:42:33 +0000

    rename some vars of heap sort.

--- java/Algorithms/src/algorithms/sort/HeapSort.java
+++ java/Algorithms/src/algorithms/sort/HeapSort.java
@@ -26,26 +26,24 @@ public class HeapSort {
         }
     }
 
-    private static void maxheapify(int a[], int i, int heapsize) {
+    private static void maxheapify(int a[], int pos, int heapsize) {
 
-        int largest;
-        int left = 2 * i;
-        int right = 2 * i + 1;
+        int largest = pos;
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
 
         // 求出 根，左子，右子之中，值最大的下标
-        if (left <= heapsize && a[left] > a[i]) {
+        if (left <= heapsize && a[left] > a[pos]) {
             largest = left;
-        } else {
-            largest = i;
-        }
+        } 
 
         if (right <= heapsize && a[right] > a[largest]) {
             largest = right;
         }
 
         // 如果不是根的值最大，则说明需要进行调整，所以要进行交换。
-        if (largest != i) {
-            ArrayUtil.swap(a, i, largest);
+        if (largest != pos) {
+            ArrayUtil.swap(a, pos, largest);
             maxheapify(a, largest, heapsize); /* recurrsion */
         }
     }
